dd41630e3541916d7116718f998454e8e2b4a68d
Kartik Dixit <kartikdixit2468@gmail.com>
2025-05-22 23:51:20 +0530

    Added middleware to check Invalid/Spam request

--- src/middlewares/validateAuthToken.js
+++ src/middlewares/validateAuthToken.js
@@ -1,17 +1,18 @@
 const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1]; // Remove "Bearer"
 
-  if (!token) return res.status(401).json({ error: "Token missing" });
+  if (!token) return res.status(401).json({ success: false, error: "Token missing" });
 
   try {
-    const user = jwt.verify(token, process.env.SECRET_KEY);
+    const user = jwt.verify(token, SECRET_KEY);
     req.user = user; // Attach user to request
     next(); // Proceed to actual route
   } catch (err) {
-    res.status(403).json({ error: "Invalid token" });
+    res.status(403).json({ success: false, error: "Invalid token" });
   }
 };
 
