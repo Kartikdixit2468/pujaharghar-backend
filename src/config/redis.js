c333aa53d40808553f7f76d097f6344ab4445e48
Kartik Dixit <kartikdixit2468@gmail.com>
2026-01-08 18:04:13 +0530

    Configured redis

---
+++ src/config/redis.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const {createClient} = require('redis');
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL
+})
+
+redisClient.on('error', (err)=>{
+    console.error("Redis Error: ", err)
+})
+
+const connectRedis = async () =>{
+    if(!redisClient.isOpen()){
+        await redisClient.connect();
+        console.log("Redis connected")
+    }
+}
+
+// module.exports = {
+//     redisClient,
+//     connectRedis
+// }
+
+
